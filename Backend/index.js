6cab95b7ed1197f0d0f1254a68c0529c72de3e3d
Hmad Afzal <hmadafzal00@gmail.com>
2024-05-04 18:21:40 +0500

    minor changes made

--- Backend/index.js
+++ Backend/index.js
@@ -1,4 +1,3 @@
-// const path = require("path");
 const express = require("express");
 
 const dotenv = require("dotenv");
@@ -6,7 +5,6 @@ dotenv.config();
 const port = process.env.PORT || 3000;
 const mongoose = require("mongoose");
 const app=express();
-// const __dirname1=path.resolve();
 mongoose
   .connect(process.env.MONGO_ATLAS_URI) 
   .then(() => {
@@ -26,21 +24,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
  
-app.use(userRouter);
-app.use(postRouter);
-app.use(replyRouter);
+
 
 app.get('/', (req,res)=>{
   res.send("running...")
 })
 
-// if(process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname1, "/Frontend/dist")));
 
-//   app.get("*" , (req,res)=>{
-//     res.sendFile(path.resolve(__dirname1, "Frontend",  "dist", "index.html"))
-//   });
-// }
+app.use(userRouter);
+app.use(postRouter);
+app.use(replyRouter);
 
 
 app.listen(port, () => {
