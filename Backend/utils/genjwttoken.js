59af5b5fc36fedcb56f92875c6d0552fd8141d25
Hmad Afzal <hmadafzal00@gmail.com>
2024-05-05 23:05:48 +0500

    test10

--- Backend/utils/genjwttoken.js
+++ Backend/utils/genjwttoken.js
@@ -7,7 +7,7 @@ const token=jwt.sign({userid},process.env.JWT_SECRET, {expiresIn:'15d'});
 res.cookie("jwt",token,{
     httpOnly: true, 
     maxAge: 15 * 24 * 60 * 60 * 1000,
-    sameSite: "strict",
+   
 } )
 return token;
 }
